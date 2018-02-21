0933f9ce10c91fba26a76daf411cfbb340e47ef1
Shruti Sharma <shrutisharma@shrutis-macbook-pro.local>
2018-02-20 16:40:48 -0800

    Initial Commit

---
+++ SpeechRecognitionAPIDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechRecognitionAPIDemo
+//
+//  Created by Shruti Sharma on 2/20/18.
+//  Copyright © 2018 Shruti Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
